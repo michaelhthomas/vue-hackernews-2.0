2464121553a90ad02f0efd22c9aedfc5b23b0587
Michael Thomas <michaelhthomas@outlook.com>
2023-05-04 16:11:57 -0400

    fix: properly load external stylesheets

--- build/webpack.server.config.js
+++ build/webpack.server.config.js
@@ -1,57 +1,52 @@
-const path = require('path')
-const webpack = require('webpack')
-const merge = require('webpack-merge')
-const base = require('./webpack.base.config')
-const nodeExternals = require('webpack-node-externals')
-const VueSSRServerPlugin = require('vue-server-renderer/server-plugin')
-
-const isProd = process.env.NODE_ENV === 'production'
+const path = require("path");
+const webpack = require("webpack");
+const merge = require("webpack-merge");
+const base = require("./webpack.base.config");
+const nodeExternals = require("webpack-node-externals");
+const VueSSRServerPlugin = require("vue-server-renderer/server-plugin");
 
 module.exports = merge(base, {
-  target: 'node',
-  devtool: '#source-map',
-  entry: './src/entry-server.js',
+  target: "node",
+  devtool: "#source-map",
+  entry: "./src/entry-server.js",
   output: {
-    filename: 'server-bundle.js',
-    libraryTarget: 'commonjs2'
+    filename: "server-bundle.js",
+    libraryTarget: "commonjs2",
   },
   resolve: {
     alias: {
-      'create-api': './create-api-server.js'
-    }
+      "create-api": "./create-api-server.js",
+    },
   },
   module: {
     rules: [
       {
         test: /\.styl(us)?$/,
         use: [
-          ...(isProd ? [
-            {
-              loader: path.resolve('build/css-extract-plugin.js')
-            }
-            ] : []),
           {
-            loader: 'css-loader',
+            loader: "css-loader",
             options: {
-                esModule: false
-            }
+              esModule: false,
+            },
           },
-          'stylus-loader'
-        ]
+          "stylus-loader",
+        ],
       },
-    ]
+    ],
   },
   // https://webpack.js.org/configuration/externals/#externals
   // https://github.com/liady/webpack-node-externals
   externals: nodeExternals({
     // do not externalize CSS files in case we need to import it from a dep
-    allowlist: /\.css$/
+    allowlist: /\.css$/,
   }),
   plugins: [
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development'),
-      'process.env.VUE_ENV': '"server"'
+      "process.env.NODE_ENV": JSON.stringify(
+        process.env.NODE_ENV || "development"
+      ),
+      "process.env.VUE_ENV": '"server"',
     }),
-    new VueSSRServerPlugin()
-  ]
-})
+    new VueSSRServerPlugin(),
+  ],
+});
